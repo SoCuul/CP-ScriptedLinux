fc11c74c3a9ef0ceb7d5aa6d084eca285fc173bf
SoCuul <63339559+SoCuul@users.noreply.github.com>
2023-10-29 21:58:53 -0700

    first commit

---
+++ main.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Check for proper elevation
+if [[ $EUID > 0 ]]
+    then echo "This script must be run as root. Exiting..."
+    exit
+fi
+
+function showMenu {
+    clear
+
+    echo "======================================================
+===  CyberPatriot Team Strawberry - Linux Scripts  ===
+======================================================
+
+1) Secure system
+2) Set Admin Accounts
+3) Delete forbidden users
+4) Set account passwords
+5) Search for files
+6) Scan for malware
+"
+}
+
+# Interactive Menu
+while true
+do
+    showMenu
+    read -p "Please make a selection: " choice
+    echo ""
+    echo "--------------------------------------------------------"
+    echo ""
+
+    case $choice in
+        1) bash ./modules/1-secureSystem.sh ;;
+        2) bash ./modules/2-setAdminAccounts.sh ;;
+        3) bash ./modules/3-removeForbiddenUsers.sh ;;
+        4) bash ./modules/4-setAccountPasswords.sh ;;
+        5) bash ./modules/5-searchForFiles.sh ;;
+        6) bash ./modules/6-scanForMalware.sh ;;
+        q) exit ;;
+        *) echo "Invalid selection. Please try again." ;;
+    esac
+
+    echo ""
+    read -p "Press enter to continue...:"
+done
---
+++ modules/1-secureSystem.sh
@@ -0,0 +1,40 @@
+# Many ideas taken (stolen) from: https://gist.github.com/bobpaw/a0b6828a5cfa31cfe9007b711a36082f
+
+read -p "Press enter to begin..."
+
+# Install updates
+apt-get update && apt-get upgrade && apt-get dist-upgrade
+
+# Configure ufw (firewall)
+apt-get install ufw && ufw allow ssh && ufw enable
+
+# Configure ssh server
+if grep -qF 'PermitRootLogin' /etc/ssh/sshd_config;
+    then sed -i 's/^.*PermitRootLogin.*$/PermitRootLogin no/' /etc/ssh/sshd_config;
+else echo 'PermitRootLogin no' >> /etc/ssh/sshd_config;
+fi
+
+# Lock root user
+passwd -l root
+
+# Change login requirements (i hope to god this works)
+sed -i 's/PASS_MAX_DAYS.*$/PASS_MAX_DAYS 90/;s/PASS_MIN_DAYS.*$/PASS_MIN_DAYS 10/;s/PASS_WARN_AGE.*$/PASS_WARN_AGE 7/' /etc/login.defs
+
+# Update PAM settings
+echo 'auth required pam_tally2.so deny=5 onerr=fail unlock_time=1800' >> /etc/pam.d/common-auth
+apt-get install libpam-cracklib
+sed -i 's/\(pam_unix\.so.*\)$/\1 remember=5 minlen=8/' /etc/pam.d/common-password
+sed -i 's/\(pam_cracklib\.so.*\)$/\1 ucredit=-1 lcredit=-1 dcredit=-1 ocredit=-1/' /etc/pam.d/common-password
+
+# Check for non-root UID 0 users
+echo "Checking for non-root UID 0 users..."
+mawk -F: '$3 == 0 && $1 != "root"' /etc/passwd
+
+# Remove anything samba related
+apt-get remove .*samba.* .*smb.*
+
+# Disable avahi
+systemctl disable avahi-daemon
+
+# Disable guest account
+echo "allow-guest=false" >> /etc/lightdm/lightdm.conf
